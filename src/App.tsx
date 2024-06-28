50d2b8fdfbcd0537bc3552185150e2e52edbf9d3
Vignesh <vignesh77024@gmail.com>
2024-06-28 17:50:48 +0530

    added date field

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import { useEffect, useState } from "react"
 import "./App.css"
 import EventItem from "./components/EventItem"
+import { Center, Container, Space, Title } from "@mantine/core"
+import { DateInput } from "@mantine/dates"
+// import "@mantine/core/styles.css"
+import "@mantine/dates/styles.css"
 
 function App() {
-  const [eventsList, setEventsList] = useState<any[]>([])
-
+  const [eventsList, setEventsList] = useState([])
+  const [date, setDate] = useState<Date | null>(null)
   useEffect(() => {
+    if (!date) return
     fetch(
-      `https://api.api-ninjas.com/v1/historicalevents?month=${12}&day=${17}`,
+      `https://api.api-ninjas.com/v1/historicalevents?month=${
+        date.getMonth() + 1
+      }&day=${date.getDate()}`,
       {
         headers: {
           "X-Api-Key": `${import.meta.env.VITE_HISTORICAL_EVENTS_API_KEY}`,
@@ -19,27 +26,47 @@ function App() {
         console.log(data)
         setEventsList(data)
       })
-  }, [])
+    console.log(date)
+  }, [date])
   return (
     <>
+      <Center>
+        <Container>
+          <Title order={1} p="lg">
+            Birthday?
+          </Title>
+          <DateInput
+            size="md"
+            w={335}
+            value={date}
+            onChange={setDate}
+            // label="Birthday?"
+            placeholder="Enter your birthday"
+            clearable
+          ></DateInput>
+        </Container>
+      </Center>
       {/* <p>{`Total No. of packages: ${totalPackages}`}</p> */}
       <div className="EventItemsContainer">
-        <EventItem
+        {/* <EventItem
           year={2000}
           month={12}
           day={17}
           text={
             "Something happened I guess what if this goes for a bit too long what if this goes for a bit too long what if this goes for a bit too longwhat if this goes for a bit too longwhat if this goes for a bit too long"
           }
-        />
+        /> */}
         {eventsList.map((event, index) => (
-          <EventItem
-            key={index} // Add a unique key prop for each item
-            year={event.year}
-            month={event.month}
-            day={event.day}
-            text={event.event}
-          />
+          <Container>
+            <Space key={index} h="md" />
+            <EventItem
+              key={index} // Add a unique key prop for each item
+              year={event.year}
+              month={event.month}
+              day={event.day}
+              text={event.event}
+            />
+          </Container>
         ))}
       </div>
     </>
