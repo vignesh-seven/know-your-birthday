c7bfbd76dde8a8c6d6d819f43128752fe25ced9a
Vignesh <vignesh77024@gmail.com>
2024-06-28 16:12:00 +0530

    add: deploy workflow

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react"
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/know-your-birthday/",
   plugins: [react()],
   // define: {
   //   "process.env": {},
