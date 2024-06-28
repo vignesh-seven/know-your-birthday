2fd195d50932caf8e31bb2f2c2b23ce5fb861487
Vignesh <vignesh77024@gmail.com>
2024-06-28 11:58:31 +0530

    config changes

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,8 +1,14 @@
 /// <reference types="vite/client" />
 
 interface EventItemProps {
-    year: number,
-    month: number,
-    day: number,
-    text: string
+  year: number
+  month: number
+  day: number
+  text: string
 }
+
+// declare const process: {
+//   env: {
+//     REACT_APP_API_KEY: string
+//   }
+// }
