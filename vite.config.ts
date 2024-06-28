df33ff9352ff71351cd997585e93fb12cf6ff15b
Vignesh <vignesh77024@gmail.com>
2024-06-28 16:34:02 +0530

    gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,8 @@ import react from "@vitejs/plugin-react"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/know-your-birthday/",
   plugins: [react()],
+  base: "/know-your-birthday/",
   // define: {
   //   "process.env": {},
   // },
