ed5f8f2c150aee48932fbd321c545f0e646d1b8c
Vignesh <vignesh77024@gmail.com>
2024-06-28 11:14:00 +0530

    fetch data from api and display them

---
+++ src/App.tsx
@@ -0,0 +1,49 @@
+import { useEffect, useState } from "react"
+import "./App.css"
+import EventItem from "./components/EventItem"
+
+function App() {
+  const [eventsList, setEventsList] = useState<any[]>([])
+
+  useEffect(() => {
+    fetch(
+      `https://api.api-ninjas.com/v1/historicalevents?month=${12}&day=${17}`,
+      {
+        headers: {
+          "X-Api-Key": "tDO8l/hSA0evAf+AGdACVA==PyA1ApDqI6G4bQeL",
+        },
+      }
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data)
+        setEventsList(data)
+      })
+  }, [])
+  return (
+    <>
+      {/* <p>{`Total No. of packages: ${totalPackages}`}</p> */}
+      <div className="EventItemsContainer">
+        <EventItem
+          year={2000}
+          month={12}
+          day={17}
+          text={
+            "Something happened I guess what if this goes for a bit too long what if this goes for a bit too long what if this goes for a bit too longwhat if this goes for a bit too longwhat if this goes for a bit too long"
+          }
+        />
+        {eventsList.map((event, index) => (
+          <EventItem
+            key={index} // Add a unique key prop for each item
+            year={event.year}
+            month={event.month}
+            day={event.day}
+            text={event.event}
+          />
+        ))}
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/EventItem.tsx
@@ -0,0 +1,38 @@
+import { Paper, Text, Flex, Box, Card, CardSection } from "@mantine/core"
+import { ReactNode } from "react"
+
+function getMonthName(month: number) {
+  const date = new Date(`2000-${month}-01`)
+
+  // Getting full month name (e.g. "September")
+  return date.toLocaleString("default", { month: "long" })
+}
+
+export default function EventItem({
+  year,
+  month,
+  day,
+  text,
+}: EventItemProps): ReactNode {
+  return (
+    <>
+      {/* <Card withBorder shadow="sm" radius="md"> */}
+      <Paper withBorder shadow="sm" radius="md">
+        <Flex justify="flex-start">
+          <Box p="md">
+            <Text>
+              {day}, {getMonthName(month)}
+            </Text>
+            <Text size="xl" fw={700}>
+              {year}
+            </Text>
+          </Box>
+          <Box w={600} p="md">
+            <Text lineClamp={20}>{text}</Text>
+          </Box>
+        </Flex>
+      </Paper>
+      {/* </Card> */}
+    </>
+  )
+}
